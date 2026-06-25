19a22cf78c6731d138f4fd9c4c35b234133820c8
ristov.ljupcho <ristov.ljupcho@gmail.com>
2026-06-25 18:59:58 +0200

    build: initialize Spring Boot API project

---
+++ src/main/java/com/loyaltapapi/loyaltapapi/LoyaltapApiApplication.java
@@ -0,0 +1,13 @@
+package com.loyaltapapi.loyaltapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyaltapApiApplication.class, args);
+    }
+
+}
